473ad9b916d90cf1169d4a677b30b72ed00c06f9
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-01-14 15:31:19 -0800

    - Files: frontend/src/main.jsx frontend/src/store/index.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 
+// Create a variable to access your store and expose it on the window.
+const store = configureStore();
+
+// should not be exposed in production
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+// wrap the rendered App component in Redux’s Provider component, passing
+// store as a prop of the same name to Provider
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );

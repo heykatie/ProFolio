40bf315ed29be35268e9c4c51a5f22041ceaedec
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-01-14 15:27:01 -0800

    - Files: frontend/src/store/index.js frontend/src/store/store.js

---
+++ frontend/src/store/store.js
@@ -0,0 +1,27 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+
+});
+
+// set to different store enhancers depending on whether the Node environment
+// is development or production
+// In production, the enhancer should only apply the thunk middleware
+let enhancer;
+if (import.meta.env.MODE === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = (await import("redux-logger")).default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+// This function will be used by main.jsx to attach the Redux
+// store to the React application
+export default configureStore;

08f891e9b97ae60330da6da5054dfd03a9fa75ac
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-01-13 23:51:52 -0800

    - Files: backend/package.json backend/utils/validation.js

---
+++ backend/utils/validation.js
@@ -0,0 +1,32 @@
+// define an Express middleware called handleValidationErrors that will call
+// validationResult from the express-validator package passing in the request.
+// If there are no validation errors returned from the validationResult function,
+// invoke the next middleware. If there are validation errors, create an error
+// with all the validation error messages and invoke the next error-handling
+// middleware.
+
+const { validationResult } = require('express-validator');
+
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = {};
+    validationErrors
+      .array()
+      .forEach(error => errors[error.path] = error.msg);
+
+    const err = Error("Bad request.");
+    err.errors = errors;
+    err.status = 400;
+    err.title = "Bad request.";
+    next(err);
+  }
+  next();
+};
+
+module.exports = {
+  handleValidationErrors
+};

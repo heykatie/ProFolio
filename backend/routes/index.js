ecc16c47f9cd7211208ab37b8255160d2be52221
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-01-14 16:24:04 -0800

    - Files: backend/routes/index.js frontend/src/store/csrf.js frontend/src/store/store.js frontend/vite.config.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,35 @@ const apiRouter = require('./api'); // import api router
 
 router.use('/api', apiRouter); // add api router to express router
 
+// In production, the XSRF-TOKEN will be attached to the index.html file in
+// the frontend/dist folder. In the backend/routes/index.js file, serve
+// the index.html file at the / route and any routes that don’t start with
+// /api. Along with it, attach the XSRF-TOKEN cookie to the response.
+
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();

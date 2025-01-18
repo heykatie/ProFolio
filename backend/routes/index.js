e205718f9eddbb1abb824b96145d531572150124
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-01-18 00:51:03 -0800

    - Files: backend/routes/index.js frontend/.gitignore frontend/dist/assets/github-6251e535.png frontend/dist/assets/google-3cc0e574.png frontend/dist/assets/index-0447b248.css frontend/dist/assets/index-23c13ec1.js frontend/dist/assets/index-26b97440.js frontend/dist/assets/index-7d3f53b3.css frontend/dist/assets/linkedin-4c9a2292.png frontend/dist/assets/profolio-icon-b2aa8a00.png frontend/dist/index.html

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -35,11 +35,21 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Add a XSRF-TOKEN cookie in development
+// if (process.env.NODE_ENV !== 'production') {
+//   router.get('/api/csrf/restore', (req, res) => {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     return res.json({});
+//   });
+// }
+
 if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
-  });
+	router.get('/api/csrf/restore', (req, res) => {
+		const csrfToken = req.csrfToken();
+		res.cookie('XSRF-TOKEN', csrfToken);
+		res.status(200).json({
+			'XSRF-Token': csrfToken,
+		});
+	});
 }
 
 // Add a XSRF-TOKEN cookie

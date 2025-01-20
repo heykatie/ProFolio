ba756e08818797d4976c8a01f2702b5c82ecfe5e
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-01-20 05:02:19 -0800

    - Files: frontend/dist/assets/index-0e23cede.js frontend/dist/assets/index-2d5f6965.css frontend/dist/assets/index-66099848.js frontend/dist/assets/index-8d7aeace.css frontend/dist/index.html frontend/src/components/FileDelete/FileDelete.jsx

--- frontend/src/components/FileDelete/FileDelete.jsx
+++ frontend/src/components/FileDelete/FileDelete.jsx
@@ -1,35 +1,35 @@
 const FileDelete = () => {
-  const extractKeyFromUrl = (url) => {
-		const parts = url.split('/');
-		return parts.slice(parts.indexOf('uploads')).join('/'); // Adjust as per your folder structure
-  };
-  
-  const handleDelete = async (fileKey) => {
-		try {
-			const response = await fetch('/api/uploads/delete', {
-				method: 'DELETE',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({ key: fileKey }),
-			});
+  // const extractKeyFromUrl = (url) => {
+	// 	const parts = url.split('/');
+	// 	return parts.slice(parts.indexOf('uploads')).join('/'); // Adjust as per your folder structure
+  // };
 
-			if (response.ok) {
-				alert('File deleted successfully');
-				// Update the UI or remove the file from the list
-			} else {
-				const { error } = await response.json();
-				alert(`Failed to delete file: ${error}`);
-			}
-		} catch (error) {
-			console.error('Error deleting file:', error);
-			alert('An error occurred while deleting the file.');
-		}
-  };
+  // const handleDelete = async (fileKey) => {
+	// 	try {
+	// 		const response = await fetch('/api/uploads/delete', {
+	// 			method: 'DELETE',
+	// 			headers: {
+	// 				'Content-Type': 'application/json',
+	// 			},
+	// 			body: JSON.stringify({ key: fileKey }),
+	// 		});
+
+	// 		if (response.ok) {
+	// 			alert('File deleted successfully');
+	// 			// Update the UI or remove the file from the list
+	// 		} else {
+	// 			const { error } = await response.json();
+	// 			alert(`Failed to delete file: ${error}`);
+	// 		}
+	// 	} catch (error) {
+	// 		console.error('Error deleting file:', error);
+	// 		alert('An error occurred while deleting the file.');
+	// 	}
+  // };
 
   return (
 		<div>
-			<ul>
+			{/* <ul>
 				{files.map((file) => (
 					<li key={file.key}>
 						<a href={file.url} target='_blank' rel='noopener noreferrer'>
@@ -38,7 +38,9 @@ const FileDelete = () => {
 						<button onClick={() => handleDelete(file.key)}>Delete</button>
 					</li>
 				))}
-			</ul>
+			</ul> */}
 		</div>
   );
 }
+
+export default FileDelete;

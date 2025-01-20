f580863784c33312a3f99f148b0ead5980ff0283
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-01-19 18:52:51 -0800

    - Files: frontend/src/components/Pages/Dashboard/Dashboard.jsx

--- frontend/src/components/Pages/Dashboard/Dashboard.jsx
+++ frontend/src/components/Pages/Dashboard/Dashboard.jsx
@@ -1,31 +1,9 @@
-import { useEffect, useState} from 'react';
-import { fetchTheme, setTheme, updateTheme } from '../../../store/theme';
-import { useSelector, useDispatch } from 'react-redux';
-
 const Dashboard = () => {
-	const sessionUser = useSelector((state) => state.session.user);
-	const dispatch = useDispatch();
-  const theme = useSelector((state) => state.theme?.theme);
-
-	useEffect( () => {
-		if (sessionUser) {
-			const localTheme = localStorage.getItem('theme') || 'light';
-    } else {
-    }
-	}, [dispatch, sessionUser]);
-
 	return (
 		<div>
 			<div>
 				<h1>Dashboard Page</h1>
 			</div>
-			<span>
-				localStorage: {localStorage.getItem('theme')}
-				<br></br>
-				shows - state/slice: {theme}
-				<br></br>
-				user: ?
-			</span>
 		</div>
 	);
 };
